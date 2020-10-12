a8e6fc02de625d949544c48f5881a43edcf60225
Geert Van Laethem <geert.vanlaethem@45degrees.be>
2020-10-12 16:21:18 +0200

    Added the solution and the first project

---
+++ src/CLI.Bootstrap.Classic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CLI.Bootstrap.Classic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
